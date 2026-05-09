48b1c2f46a7d40164497d556c1e972a53c4858ea
Andy Smircich <smirciat@hotmail.com>
2026-05-08 18:52:13 -0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
 apps : [{
 	name: "goldPoints",
 	script: "/home/andy/goldPoints/dist/server",
-	interpreter: "/home/andy/.nvm/versions/node/v10.24.1/bin/node",
 	env: {
 		NODE_ENV: "production"
 	}
