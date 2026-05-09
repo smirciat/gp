47098838d747bf1c702d15f8973e23c55fb4c038
Andy Smircich <smirciat@hotmail.com>
2026-05-08 17:38:02 -0800

    ecosystem rename

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
 apps : [{
-	name: "fraBering",
+	name: "goldPoints",
 	script: "/home/andy/goldPoints/dist/server",
 	interpreter: "/home/andy/.nvm/versions/node/v10.24.1/bin/node",
-    node_args: "--max-old-space-size=4096",
 	env: {
 		NODE_ENV: "production"
 	}
