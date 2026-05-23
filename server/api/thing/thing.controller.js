d881506250d319f4bb6210b453810f167ce600a3
Andy Smircich <smirciat@hotmail.com>
2026-05-23 08:42:04 -0800

    formatting

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -338,7 +338,7 @@ export async function setBearer(){
       "name": localEnv.WEBHOOK_HANDLER,
       "url": localEnv.WEBHOOK_URL,
       "events": [
-        "*",//"Takeflite.Operations.AircraftControl.FlightStatusChanged"//or "*"
+        "Takeflite.Operations.AircraftControl.FlightStatusChanged"//or "*"
       ],
       "deliveryAttributeMapping": [
         {
