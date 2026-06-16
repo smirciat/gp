512c982cae5ea4998e13ab353034e162f6e6f09d
Andy Smircich <smirciat@hotmail.com>
2026-06-16 14:32:55 -0800

    first and last name

--- server/api/transaction/transaction.controller.js
+++ server/api/transaction/transaction.controller.js
@@ -326,6 +326,19 @@ async function flightCompleted(flight){
       if (passenger.boardPoint.name!==origin) return;
       passenger.description = dateString + ' ' + passenger.bookingNumber + ' ' + passenger.boardPoint.code + '-' + passenger.offPoint.code;
       passenger.description += ' ' + f.flightNumber + ' Assigned after Takeflite Webhook';
+      if (passenger.name) {
+        let arr=[];
+        if (!passenger.name.firstName&&passenger.name.lastName) {
+          arr=passenger.name.lastName.split(' ');
+          passenger.name.firstName=arr.shift();
+          passenger.name.lastName=arr.join(' ');
+        }
+        if (passenger.name.firstName&&!passenger.name.lastName) {
+          arr=passenger.name.firstName.split(' ');
+          passenger.name.firstName=arr.shift();
+          passenger.name.lastName=arr.join(' ');
+        }
+      }
       passengers.push(passenger);
     });
     manifest.passengers=passengers;
