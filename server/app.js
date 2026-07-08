54a24b2be0b22b90e0a95008bd98066b3ab507bd
Andy Smircich <smirciat@hotmail.com>
2026-07-07 21:53:51 -0800

    stop load

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ function startServer() {
   app.angularFullstack = server.listen(config.port, config.ip, function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
     setBearer();
-    load();
+    //load();
     //loadEmailCSV();
   });
 }
